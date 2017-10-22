022f4510f9cd1af352b2b1bb9f18c8d11f2bfcd7
hongjie <32968210@qq.com>
2017-10-22 22:03:26 +0800

    增加了前端所需要的一些接口

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const router            = require('koa-router')();
-const config            = require('../config');
-const user    = require("../app/controllers/user");
+const router = require('koa-router')();
+const config = require('../config');
+const user   = require("../app/controllers/user");
 
 router.get('/', function (ctx, next) {
 	ctx.body = 'this a users response!';
 });
 
-router.get('register/:account/:password/:phone/:source', user.register);
+// router.get('register/:account/:password/:phone/:source', user.register);
 
-router.get('login/:account/:password', user.login);
+router.get('login/:account/:source', user.login);
+
+router.get('levelWin/:account/:level/:startTimer/:endTimer/:startNumItem1/:startNumItem2/:startNumItem3/:startNumItem4/:endNumItem1/:endNumItem2/:endNumItem3/:endNumItem4/:startDollar/:endDollar/:endDiamonds', user.levelWin);
 
 module.exports = router;
